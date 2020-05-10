cf0b96e0e65e0a9e4f5f0976e0efe49d7900f5e3
Will Fong <willfong@pm.me>
2020-05-10 09:40:37 +0800

    Used wrong index

--- list_instances.py
+++ list_instances.py
@@ -9,6 +9,6 @@ def _read_tag(tags, key):
         if tag['Key'] == key:
             return tag['Value']
 
-for instance in response['Reservations'][1]['Instances']:
+for instance in response['Reservations'][0]['Instances']:
     print(f"{instance['InstanceId']}\t{instance['InstanceType']}\t{_read_tag(instance['Tags'], 'Name')}\t{instance['PublicDnsName']}")
 
